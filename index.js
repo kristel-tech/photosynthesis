acc09c05038da20d1650fe6dd1ac377a74b20c0e
Wesley Earl Stander <wesleys@bbd.co.za>
2021-05-18 00:17:13 +0200

    feat: added authentication

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var express = require('express');
-var app = express();
 const inports = require("./data/testdata.json");
+const express = require('express');
+const open = require('open');
+require('./jsfiles/Amplify');
+const Amplify = require('aws-amplify');
+let app = express();
+//const inports = require("data/testdata.json");
 let port = process.env.PORT || 3000;
+let user = null;
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -10,11 +15,26 @@ app.use(express.static("jsfiles"));
 app.use(express.static("audio"));
 
 app.get('/', (req, res) => {
+    if (user == null) {
+      Amplify.Auth.federatedSignIn().then(cred => {
+        return Amplify.Auth.currentCredentials();
+      }).then(u => {
+        user = u;
+        console.log(u);
+      }).catch(e => {
+        console.log(e);
+      });
+    }
     res.render("pages/index");
 });
 
-app.get('/ranks', (req, res) => {
-    res.send(inports);
+app.get('/callback', (req, res) => {
+    res.render("pages/index");
+});
+
+app.get('/signOut', (req, res) => {
+    Amplify.Auth.signOut({global: true});
+    open("/");
 });
 
 app.listen(port, () => {
--- jsfiles/Amplify.js
+++ jsfiles/Amplify.js
@@ -1,77 +1,24 @@
+const Amplify = require('aws-amplify');
+
 const currentConfig = {
    Auth: {
-    // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
-    //identityPoolId: 'us-east-2:524648211309:userpool',
-
-    // REQUIRED - Amazon Cognito Region
+    identityPoolId: 'us-east-2:71cce9c1-d659-4cea-afb6-630451d4f737',
     region: 'us-east-2',
-
-    // OPTIONAL - Amazon Cognito Federated Identity Pool Region
-    // Required only if it's different from Amazon Cognito Region
-    //identityPoolRegion: 'XX-XXXX-X',
-
-    // OPTIONAL - Amazon Cognito User Pool ID
     userPoolId: 'us-east-2_MGBIbIiVA',
-
-    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-    userPoolWebClientId: '66mhrtl17hf7i7dode9r5o9528',
-
-    // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+    userPoolWebClientId: '3jor64kidr6sg3pbihjlam6ahh,5hqt6f4rt8bv5p3c34na39fl4j',
     mandatorySignIn: false,
-
-    // OPTIONAL - Configuration for cookie storage
-    // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
-    // cookieStorage: {
-    // // REQUIRED - Cookie domain (only required if cookieStorage is provided)
-    //     domain: '.yourdomain.com',
-    // // OPTIONAL - Cookie path
-    //     path: '/',
-    // // OPTIONAL - Cookie expiration in days
-    //     expires: 365,
-    // // OPTIONAL - See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
-    //     sameSite: "strict" | "lax",
-    // // OPTIONAL - Cookie secure flag
-    // // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
-    //     secure: true
-    // },
-
-    // OPTIONAL - customized storage object
-    //storage: MyStorage,
-
-    // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
     authenticationFlowType: 'USER_PASSWORD_AUTH',
-
-    // OPTIONAL - Manually set key value pairs that can be passed to Cognito Lambda Triggers
-    //clientMetadata: { myCustomKey: 'myCustomValue' },
-
-     // OPTIONAL - Hosted UI configuration
     oauth: {
-        domain: 'photosythesis.auth.us-east-2.amazoncognito.com',
-        scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
-        redirectSignIn: 'https://photosynthesynth.herokuapp.com/',
-        redirectSignOut: 'https://photosynthesynth.herokuapp.com/',
-        responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
+      domain: 'photosythesis.auth.us-east-2.amazoncognito.com',
+      scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
+      redirectSignIn: 'http://localhost:3000/callback,https://photosynthesynth.herokuapp.com/callback',
+      redirectSignOut: 'http://localhost:3000/,https://photosynthesynth.herokuapp.com/',
+      responseType: 'code'
     }
   },
-  API: {
-       endpoints: [
-           {
-               name: "test",
-               endpoint: "https://rftoua44qf.execute-api.us-east-2.amazonaws.com",
-           }
-       ]
-   }
-}
+};
 
-const isLocalhost = Boolean(
-  window.location.hostname === "localhost" ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === "[::1]" ||
-    // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
-);
+const isLocalhost = true;
 
 const [
   localRedirectSignIn,
@@ -83,10 +30,16 @@ const [
   productionRedirectSignOut,
 ] = currentConfig.Auth.oauth.redirectSignOut.split(",");
 
+const [
+  localAppClient,
+  productionAppClient,
+] = currentConfig.Auth.userPoolWebClientId.split(",");
+
 const updatedAwsConfig = {
   ...currentConfig,
   Auth: {
     ...currentConfig.Auth,
+    userPoolWebClientId: isLocalhost ? localAppClient : productionAppClient,
     oauth: {
       ...currentConfig.Auth.oauth,
       redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
@@ -95,4 +48,6 @@ const updatedAwsConfig = {
   }
 }
 
-export default updatedAwsConfig;
+Amplify.Amplify.configure(updatedAwsConfig);
+
+module.exports = { updatedAwsConfig }
