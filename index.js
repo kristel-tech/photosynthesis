e17048c4e6bac2ccacc9b0437bab213e31adc57e
Wesley Earl Stander <wesleys@bbd.co.za>
2021-05-20 16:35:47 +0200

    feat: changed to auth0

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
-require('./jsfiles/Amplify');
-const Amplify = require('aws-amplify');
 const path = require('path');
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'https://photosynthesynth.herokuapp.com',
+  clientID: 'C9v7Jr7OGbnCvllmy5LqXtXadkISa59m',
+  issuerBaseURL: 'https://dev-70k6l87u.us.auth0.com'
+};
+
 let app = express();
 //const inports = require("data/testdata.json");
 let port = process.env.PORT || 3000;
@@ -14,6 +23,14 @@ app.use(express.static("jsfiles"));
 app.use(express.static("audio"));
 //app.use(express.static(path.join(__dirname, 'public')));
 
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
 app.get('/', (req, res) => {
     console.log(user);
     if (user === null) {
@@ -22,7 +39,7 @@ app.get('/', (req, res) => {
         return Amplify.Auth.currentCredentials();
       }).then(u => {
         user = u.sessionToken;
-        console.log("Token:" + u.sessionToken);
+        console.log(u);
       }).catch(e => {
         res.render("failed to login. f5");
       });
