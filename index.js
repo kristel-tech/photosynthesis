363041cfd7a3209de727564d6baee052e3daaee2
Wesley Earl Stander <wesleys@bbd.co.za>
2021-05-20 15:36:19 +0200

    feat: sign out when app opens

--- index.js
+++ index.js
@@ -15,15 +15,18 @@ app.use(express.static("audio"));
 //app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
+    console.log(user);
     if (user === null) {
-      console.log("attempted sign in");
-      Amplify.Auth.federatedSignIn().then(cred => {
-        return Amplify.Auth.currentCredentials();
-      }).then(u => {
-        user = u;
-        console.log(u);
-      }).catch(e => {
-        res.render("failed to login. f5");
+      Amplify.Auth.signOut({global: true}).then(() => {
+        console.log("attempted sign in");
+        Amplify.Auth.federatedSignIn().then(cred => {
+          return Amplify.Auth.currentCredentials();
+        }).then(u => {
+          user = u;
+          console.log(u);
+        }).catch(e => {
+          res.render("failed to login. f5");
+        });
       });
     } else {
       res.render("pages/index");
