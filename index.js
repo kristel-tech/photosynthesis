7f5068552a55c25ea2b8316cb9fd9f867cc23455
Wesley Earl Stander <wesleys@bbd.co.za>
2021-05-20 15:51:43 +0200

    hotfix

--- index.js
+++ index.js
@@ -17,16 +17,14 @@ app.use(express.static("audio"));
 app.get('/', (req, res) => {
     console.log(user);
     if (user === null) {
-      Amplify.Auth.signOut({global: true}).then(() => {
-        console.log("attempted sign in");
-        Amplify.Auth.federatedSignIn().then(cred => {
-          return Amplify.Auth.currentCredentials();
-        }).then(u => {
-          user = u;
-          console.log(u);
-        }).catch(e => {
-          res.render("failed to login. f5");
-        });
+      console.log("attempted sign in");
+      Amplify.Auth.federatedSignIn().then(cred => {
+        return Amplify.Auth.currentCredentials();
+      }).then(u => {
+        user = u;
+        console.log(u);
+      }).catch(e => {
+        res.render("failed to login. f5");
       });
     } else {
       res.render("pages/index");
