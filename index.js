cce12bb081816625052ac4ad25a1b0b0e42279a2
bbdnet2142 <khutjom@bbd.co.za>
2021-05-21 10:11:00 +0200

    putting the index.js in line

--- index.js
+++ index.js
@@ -1,26 +1,97 @@
-var express = require('express');
-var app = express();
-const inports = require("./data/testdata.json");
+const express = require('express');
+const connect = require("./database/databasesetup.js");
+const path = require('path');
+// const { auth, requiresAuth } = require('express-openid-connect');
+let app = express();
+
+// const config = {
+//     authRequired: true,
+//     auth0Logout: true,
+//     secret: 'a long, randomly-generated string stored in env',
+//     baseURL: 'https://photosynthesynth.herokuapp.com',
+//     clientID: 'C9v7Jr7OGbnCvllmy5LqXtXadkISa59m',
+//     issuerBaseURL: 'https://dev-70k6l87u.us.auth0.com'
+// };
+
+
 let port = process.env.PORT || 3000;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+app.use(express.static("css"));
 app.use(express.static("jsfiles"));
 app.use(express.static("audio"));
-app.use(express.static("css"));
+app.use(express.static("assets"));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// app.use(auth(config));
 
 app.get('/', (req, res) => {
+
     res.render("pages/index");
-    // res.render("pages/oscillator");
 });
 
 
-app.get('/ranks', (req, res) => {
-    res.send(inports);
+// TODO use correct request method needs to be PUT
+app.put('/addconfig', (req, res) => {
+    let dbconnectinst = new connect();
+    dbconnectinst.SetConfigData(req, res, '/addconfig');
+});
+
+// TODO use correct request method needs to be PUT
+app.put('/addconfig', (req, res) => {
+    let dbconnectinst = new connect();
+    dbconnectinst.SetConfigData(req, res, '/addconfig');
+});
+
+// TODO use correct request method needs to be POST
+app.post('/getconfig', (req, res) => {
+    let dbconnectinst = new connect();
+    dbconnectinst.GetConfigData(req, res, '/getconfig');
+});
+
+// TODO use correct request method needs to be PUT
+app.post('/updateconfig', (req, res) => {
+    let dbconnectinst = new connect();
+    dbconnectinst.UpdateConfigData(req, res, '/updateconfig');
+});
+
+// TODO use correct request method needs to be DELETE
+app.put('/deleteconfig', (req, res) => {
+    let dbconnectinst = new connect();
+    dbconnectinst.DeleteConfigData(req, res, '/deleteconfig');
+});
+
+
+
+
+
+// app.get("/synth", requiresAuth(), (req, res) => {
+//     res.render("pages/index");
+// });
+
+// app.get('/profile', requiresAuth(), (req, res) => {
+//     res.send(JSON.stringify(req.oidc.user));
+// });
+
+app.get('/callback', (req, res) => {
+    res.redirect("/synth");
+});
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect("/");
 });
 
 app.listen(port, () => {
 
-    console.log('Server is listening on port 3000');
+    console.log('Server is listening on port ' +
+        port);
 });
